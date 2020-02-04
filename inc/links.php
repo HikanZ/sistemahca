cac74604d68acc4332a26fdc1f9d83dd0ce254fb
HikanZ <hitoshi.kanashiro@gmail.com>
2020-02-04 15:53:31 -0300

    Add files via upload

---
+++ inc/links.php
@@ -0,0 +1,34 @@
+<?php
+  $linkmenu = "window.location.href='index.php'";
+  $linkusers = "window.location.href='usuarios.php'";
+  $linksystem = "window.location.href='sistema.php'";
+  $linkrop = "window.location.href='rop.php'";
+  $linkaudit = "window.location.href='audit.php'";
+  $linkreport = "window.location.href='404.php'";
+  $linkacc = "window.location.href='minha-acc.php'";
+
+	$linkadduser = "window.location.href='usuarios-cadastro.php'";
+	$linksearchuser = "window.location.href='usuarios-pesquisa.php'";
+	$linkaccessuser = "window.location.href='usuarios-acesso.php'";
+  $linkaccvisualize = "window.location.href='visualizar-acc.php'";
+
+  $linkropadd = "window.location.href='rop-ano.php'";
+  $linkropremove = "window.location.href='rop-remove.php'";
+  $linkroplist = "window.location.href='rop-list.php'";
+
+  $linkropgroup = 'rop-grupo.php';
+  $linkropfinal = 'rop-final.php';
+
+  $linksetoradd = "window.location.href='setor-cadastro.php'";
+  $linksetorremove = "window.location.href='setor-remove.php'";
+  $linksetorlist = "window.location.href='setor-lista.php'";
+
+  $linksetoraddp = '';
+
+  $linkaccavatar = "window.location.href='minha-acc-avatar.php'";
+  $linkaccpassword = "window.location.href='minha-acc-senha.php'";
+
+  $linkpages = "window.location.href='404.php'";
+  $linkdatabase = "window.location.href='404.php'";
+  $linknumbers = "window.location.href='404.php'";
+?>
