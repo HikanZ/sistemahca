6a91c55d02b7dcec1776bcf43e2bacece049e8ea
HikanZ <hitoshi.kanashiro@gmail.com>
2020-02-06 00:23:58 -0300

    Update usuarios-pesquisa.php

--- usuarios-pesquisa.php
+++ usuarios-pesquisa.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br" class="">
-
+<?php $_SERVER['REQUEST_URI'] ?>
 <head>
 	<!-- Mobile Specific Meta -->
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
