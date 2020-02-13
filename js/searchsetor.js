6771448c6793765abbdf7f4edf5f037c2a97b32c
HikanZ <hitoshi.kanashiro@gmail.com>
2020-02-13 20:20:41 -0300

    Add files via upload

--- js/searchsetor.js
+++ js/searchsetor.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   	// TRIGGERS FOR THE SEARCH FUNCTION AFTER USER UPDATES SEARCH VALUES
-    $('#search-val-name').keyup(function() {
+    $('#search-val-name, #search-val-name2').keyup(function() {
         checkAll();
     });
   	$('#primary-switch').change(function(){
@@ -23,9 +23,10 @@ $(document).ready(function() {
         $('#resultTable tr:not(:first-child)').each(function(i, item) {
             if (
               	// 1.) check for text match...
-                $(this).find('td:eq(0)').text().toLowerCase().indexOf($('#search-val-name').val().toLowerCase()) >= 0 &&
+                $(this).find('td:eq(0)').text().toLowerCase().indexOf($('#search-val-name2').val().toLowerCase()) >= 0 &&
+                $(this).find('td:eq(1)').text().toLowerCase().indexOf($('#search-val-name').val().toLowerCase()) >= 0 &&
                 // 3.) check for presence of a value in cell
-                !$(this).find('td:eq(1)').is(":empty") == $('input[name="searchvalhasphone"]').prop('checked')
+                !$(this).find('td:eq(2)').is(":empty") == $('input[name="searchvalhasphone"]').prop('checked')
             ) {
                 $(this).show();
             } else {
